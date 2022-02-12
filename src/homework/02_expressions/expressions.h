1eb54d7b4d51ea62e8d89b14b201e10de6c02ec7
Viviannaa <viviana.gutierrez@g.austincc.edu>
2022-02-11 23:17:14 -0600

    hw 2 with test cases

--- src/homework/02_expressions/expressions.h
+++ src/homework/02_expressions/expressions.h
@@ -1,8 +1,16 @@
-﻿//example
-int add_numbers(int num1, int num2);
+﻿
+//example
+
+/* int add_numbers(int num1, int num2);
+*/
 
 //write function prototype for multiply_numbers with two integer parameters num1, and num2
 
 
+const double tax_rate = 6.75; //out tax rate
 
+int decimal = 100; // used to convert to whole number
 
+double get_sales_tax_amount( double meal_amount);
+
+double get_tip_amount (double meal_amount, double tip_rate);
