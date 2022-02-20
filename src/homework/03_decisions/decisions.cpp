045ff2066b015d2a956955eed131a1ce53f53b88
Viviannaa <viviana.gutierrez@g.austincc.edu>
2022-02-20 00:36:24 -0600

    hw_3_with_test

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,5 @@
 #include "decisions.h"
+
 //write include statement for decisions header
 
 //Write code for function get_grade_points that accepts a string letter_grade and returns 
@@ -9,40 +10,61 @@
 //given grade "D" returns 1
 //given grade "F" returns 0
 //another other value return -1
-int get_grade_points(std::string letter_grade)
+
+
+
+
+std::string get_grade_points_using_if( int grade)
 {
-	if (letter_grade == "A") 
+	std::string letter_grade;
+	if (grade <= 100 and grade >= 90) 
 	{
-		return 4;
+		letter_grade =  "A";
 	}
-	else if(letter_grade == "B") 
+	else if(grade >= 80) 
 	{
-		return 3;
+		letter_grade = "B";
 	}
-	else if (letter_grade == "C")
+	else if (grade >= 70)
 	{
-		return 2;
+		letter_grade ="C";
 	}
-	else if (letter_grade == "D")
+	else if (grade >= 60)
 	{
-		return 1;
+		letter_grade = "D";
 	}
-	else if (letter_grade == "F")
+	else
 	{
-		return 0;
+		letter_grade = "F";
 	}
-	else 
-	{
-		return -1;
-	}
+	return letter_grade;
 }
 
 
-//Write code for function calculate_gpa that accepts an int named credit_hours and
-//a double named credit_points.  The function returns the quotient of credit_points divided by
-//credit_hours. In the function account for division by zero by returning a -1.
-double calculate_gpa(int credit_hours, double credit_points)
-{
-	return credit_points / credit_hours;
-}
+std::string get_letter_grade_using_switch(int grade)
+{ 
+	std::string letter_grade;
+	int ten = 10;
 
+	grade = grade/ten;
+	
+	switch (grade)
+	{
+		case 9 or 1:
+			letter_grade = "A";
+			break;
+		case 8:
+			letter_grade = "B"; 
+			break;
+		case 7: 
+			letter_grade = "C";
+			break;
+		case 6: 
+			letter_grade = "D";
+			break;
+		default:
+			letter_grade = "F";
+			break;
+	}
+	return letter_grade;
+}
