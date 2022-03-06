82eb640e9c5d9314255cac1ac974fabe43c802e0
Viviannaa <viviana.gutierrez@g.austincc.edu>
2022-03-05 23:20:03 -0600

    hw 5

--- src/classwork/05_assign/sequence.cpp
+++ src/classwork/05_assign/sequence.cpp
@@ -1,3 +1,111 @@
+#include "sequence.h"
+
+
+
+
+double get_gc_content(const string& dna)
+{
+    double dna_size, count, gc_percent;
+    dna_size = dna.size();
+    for(auto& ch: dna)
+    {
+        if (ch == 'G' || ch == 'C')
+        {
+            count++;
+        }
+    }
+    gc_percent = count / dna_size;
+
+    return gc_percent;
+
+}
+
+string reverse_string(string dna)
+{
+    string test_var;
+    for(int i = dna.size()-1; i >=0; i--)
+    {
+        test_var += dna [i];
+    }
+    return test_var;
+
+}
+
+std::string get_dna_complement(std::string dna)
+{
+    
+    string reverse_dna = reverse_string(dna);
+    for(char i = reverse_dna.size()-1; i >= 0; i--)
+    {
+        switch(reverse_dna[i])
+        {
+            case 'G':
+                reverse_dna[i] = 'C';
+                break;
+            case 'C':
+                reverse_dna[i] = 'G';
+                break;
+            case 'T':
+                reverse_dna[i] = 'A';
+                break;
+            case 'A':
+                reverse_dna[i] = 'T';
+                break;
+        }
+    }
+
+    return reverse_dna;
+}
+
+void display_menu()
+{
+    cout << "\n--MENU--\n";
+    cout << "1 -- get GC conent \n";
+    cout << "2 -- get DNA completment\n";
+    cout << "3 -- Exit \n";
+}
+
+int run_menu()
+{
+    int choice;
+    cout << "enter choice from menu: \n";
+        cin >> choice;
+    return choice;
+}
+
+void user_menu_options(int choice)
+{
+    string dna_test, result_string;
+    double result_doub;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "enter dna string: ";
+        cin >> dna_test;
+        result_doub = get_gc_content(dna_test);
+        cout << "percent in decimal form is: "<< result_string << "\n";
+
+        break;
+    case 3:
+       break;
+    
+    default:
+    cout << "choice entered is not valid.";
+        break;
+        
+    }
+    
+}
+
+// ask if user wants to exit / comfirm
+char user_confirm_option()
+{
+    char option_confirmed;
+    cout << "are you sure you want to exit this program?\n type y or n: ";
+    cin >> option_confirmed;
+    return option_confirmed;
+}
 //write include statements 
 
 
--- test/classwork_test/05_assign_test/sequence_tests.cpp
+++ test/classwork_test/05_assign_test/sequence_tests.cpp
@@ -1,8 +1,30 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
-#include "loops.h"
+#include <iostream>
+
+#include "sequence.h"
+#include "/src/classwork/05_assign"
+
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test gc function")
+{
+	REQUIRE(get_gc_content("AGCTATAG") == .375);
+	REQUIRE(get_gc_content("CGCTATAG") == .50);
+}
+TEST_CASE("Test reverse string function")
+{
+	REQUIRE("GATATCGA" == reverse_string("AGCTATAG"));
+	REQUIRE("GATATCGC" == reverse_string("CGCTATAG"));
+
+}
+
+TEST_CASE("Test dna complement function")
+{
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+
 }
 
